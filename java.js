d710a991b4e90efca07771824eda9e5faac519ed
Fran-jeanne-sanchez <francisca.sanchez@genstudents.org>
2024-05-07 14:18:04 -0400

    cambios

--- java.js
+++ java.js
@@ -7,26 +7,26 @@ let numero3 = prompt("IIngrese el tercer numero");
 numero1 = parseFloat(numero1);
 numero2 = parseFloat(numero2);
 numero3 = parseFloat(numero3);
-if (numero1 === numero2 && numero1 === numero3){
+if (numero1 === numero2 && numero2 === numero3){
     console.log("Los numeros son iguales")
 } else{
 
-let mayor, centro, menor;
+ let mayor, centro, menor;
 
-if (numero1 >= numero2 && numero1 >= numero3){
+ if (numero1 >= numero2 && numero1 >= numero3){
     mayor = numero1;
     centro = Math.max(numero2, numero3);
     menor = Math.min(numero2, numero3);
-} else if (numero2 >= numero1 && numero2 >= numero3){
+ } else if (numero2 >= numero1 && numero2 >= numero3){
     mayor = numero2;
     centro = Math.max(numero1, numero3);
     menor = Math.min(numero1, numero3);
-}else {
+ }else {
     mayor = numero3;
     centro = Math.max(numero1, numero2);
     menor = Math.min(numero1, numero2);
 
-}
-console.log(`Ordenalos de mayor a menor: ${mayor}, ${centro}, ${menor}` );
-console.log(`Ordenalos de menor a mayor: ${mayor}, ${centro}, ${menor}` );
+ }
+ console.log(`Ordenalos de mayor a menor: ${mayor}, ${centro}, ${menor}` );
+ console.log(`Ordenalos de menor a mayor: ${mayor}, ${centro}, ${menor}` );
 }
